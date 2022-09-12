4c68ac7b943eb1e59848832c158cbaf9b9c3ee32
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-09-12 18:36:34 +0100

    loop throught the POSST
    wrapped the Posts in a scroolview

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import { SafeAreaView,StyleSheet, Text, View } from 'react-native'
+import {SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react';
 import Header from '../components/home/Header';
 import Stories from '../components/home/Stories';
-
+import Post from '../components/home/Post';
+import { POSTS } from '../data/posts'
 
 
 const HomeScreen = () => {
@@ -11,6 +12,14 @@ const HomeScreen = () => {
       {/* Header */}
       <Header />
       <Stories />
+      <ScrollView>
+        {
+          POSTS.map((post, index) => (
+              <Post post={post} key={index} />
+            ))
+        }
+      </ScrollView>
+      
     </SafeAreaView>
   )
 }
