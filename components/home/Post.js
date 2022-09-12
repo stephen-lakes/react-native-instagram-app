f979ef0c341279ba8db80f7fbba2e95f01f96dda
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-09-12 20:39:53 +0100

    post footer icons
    listed postFooterIcons for the postFooterIcons

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,11 +1,32 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+const postFooterIcons = [
+    {
+        name: 'Like',
+        imageUrl: 'https://img.icons8.com/material-outlined/24/FFFFFF/filled-like.png',
+        likedImageUrl: '',
+    },
+    {
+        name: 'Comment',
+        imageUrl: 'https://img.icons8.com/external-anggara-basic-outline-anggara-putra/24/FFFFFF/external-comment-ecommerce-interface-anggara-basic-outline-anggara-putra.png'
+    },
+    {
+        name: 'Share',
+        imageUrl: 'https://img.icons8.com/external-outline-juicy-fish/60/FFFFFF/external-send-arrows-outline-outline-juicy-fish.png'
+    },
+    {
+        name: 'Save',
+        imageUrl: '"https://img.icons8.com/sf-regular/48/FFFFFF/bookmark-ribbon.png'
+    }
+]
+
 const Post = ({ post }) => {
   return (
     <View style={{ marginBottom: 30 }}>
         <Text style={styles.divider}></Text>
         <PostHeader post={post} />
+        <PostImage post={post} />
     </View>
   )
 }
@@ -23,6 +44,15 @@ const PostHeader = ({ post }) =>(
             <Text style={{ color: '#fff', marginLeft: 5, fontWeight: '700' }}>{ post.user }</Text>
         </View>
         <View style={{ color: 'white', fontWeight: '900'}}>...</View>
+    </View>
+)
+
+const PostImage = ({ post }) => (
+    <View style={{ height: 450, width: '100%'}}>
+        <Image 
+            source={{ uri: post.imageUrl }} 
+            style={{ height: '100%', resizeMode: 'cover' }} 
+        />
     </View>
 )
 
