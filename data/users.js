7697a69b5b122e550847c87fb24e2b9b030513b6
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-09-12 14:03:37 +0100

    added data folder
    data store for the application
    
    added users.js to store users data

---
+++ data/users.js
@@ -0,0 +1,32 @@
+export const USERS = [
+    {
+        user: 'SteveCrown',
+        image:
+            'https://pbs.twimg.com/profile_images/1355437024753438721/rxW732_w_400x400.jpg'
+    },
+    {
+        user: 'Peace Itimi',
+        image:
+            'https://pbs.twimg.com/profile_images/1498184573901017093/nSqiczzW_400x400.jpg'
+    },
+    {
+        user: 'taylor desseyn',
+        image:
+            'https://pbs.twimg.com/profile_images/1489351884443897861/zCNLirGY_400x400.jpg'
+    },
+    {
+        user: 'Jess Chan',
+        image:
+            'https://pbs.twimg.com/profile_images/1237741639193571328/LryLEBqW_400x400.jpg'
+    },
+    {
+        user: 'DesignQueen',
+        image:
+            'https://pbs.twimg.com/profile_images/1562164716948054017/KthpRY42_400x400.jpg'
+    },
+    {
+        user: 'BlackDev',
+        image:
+            'https://pbs.twimg.com/profile_banners/992926239471087616/1655918076/600x200'
+    },
+]
