d85b5bd6e0c0a8b9bf533197ecef40446cb7d258
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-09-12 13:58:07 +0100

    added stories component
    created Stories component

---
+++ components/home/Stories.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Stories = () => {
+  return (
+    <View>
+      <Text style={{ color: '#fff'}}>Stories</Text>
+    </View>
+  )
+}
+
+export default Stories
+
+const styles = StyleSheet.create({})
