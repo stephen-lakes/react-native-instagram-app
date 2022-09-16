e5d7ceecf72589da724a55a8149bde36327fd089
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-09-16 21:21:55 +0100

    added icons folder to store project icons
    added the bottomTabIcons

---
+++ icons/bottomtab.js
@@ -0,0 +1,27 @@
+export const BOTTOMTABICONS = [
+    {
+        name: 'Home',
+        active: 'https://img.icons8.com/fluency-systems-filled/24/FFFFFF/home.png',
+        inactive: 'https://img.icons8.com/fluency-systems-regular/24/FFFFFF/home.png'
+    },
+    {
+        name: 'Search',
+        active: 'https://img.icons8.com/external-anggara-glyph-anggara-putra/24/FFFFFF/external-search-social-media-interface-anggara-glyph-anggara-putra.png',
+        inactive: 'https://img.icons8.com/ios/50/FFFFFF/search--v1.png'
+    },
+    {
+        name: 'Reels',
+        active: 'https://img.icons8.com/ios-filled/50/FFFFFF/instagram-reel.png',
+        inactive: 'https://img.icons8.com/ios/24/FFFFFF/instagram-reel.png'
+    },
+    {
+        name: 'Shop',
+        active: 'https://img.icons8.com/ios-glyphs/24/FFFFFF/shopping-bag-full.png',
+        inactive: 'https://img.icons8.com/windows/24/FFFFFF/shopping-bag-full.png'
+    },
+    {
+        name: 'Profile',
+        active: 'https://pbs.twimg.com/profile_images/1355437024753438721/rxW732_w_400x400.jpg',
+        inactive: 'https://pbs.twimg.com/profile_images/1355437024753438721/rxW732_w_400x400.jpg',
+    },
+]
